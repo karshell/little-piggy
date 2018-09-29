5cc6c270e7ea4fbed035ed7d55e3c9df05bbb386
Kevin Shelley <kshelley97@gmail.com>
2018-09-29 16:13:50 -0400

    basic header component

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SummaryComponent } from './components/summary.component'
+import { SummaryComponent } from './components/summary.component';
+import { GraphComponent } from './components/graph.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SummaryComponent
+    SummaryComponent,
+    GraphComponent
   ],
   imports: [
     BrowserModule
---
+++ angular/src/app/components/graph.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'graph',
+  templateUrl: './graph.component.html',
+  styleUrls: ['./graph.component.css']
+})
+
+export class GraphComponent {
+    
+}
